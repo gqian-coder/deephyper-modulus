d9ce72dcb7baf8ede0460030f9a45e113090528c
Qian Gong <gq7@ornl.gov>
2024-01-25 16:37:09 -0500

    fix a bug on calling evaluator function on CUDA GPU

--- darcy_fno_dhp/train_fno_darcy_dhp.py
+++ darcy_fno_dhp/train_fno_darcy_dhp.py
@@ -1,3 +1,4 @@
+import mpi4py
 from modulus.models.fno import FNO
 from modulus.datapipes.benchmarks.darcy import Darcy2D
 from modulus.utils import StaticCaptureTraining, StaticCaptureEvaluateNoGrad
@@ -32,16 +33,16 @@ def run(job: RunningJob):
         latent_channels=param["latent_channels"],
         num_fno_layers=param["fno_layers"],
         num_fno_modes=param["fno_modes"],
-        padding=param["padding"],
+        padding=cfg.arch.fno.fno_modes,#param["padding"],
     ).to(dist.device)
 
     loss_fun  = torch.nn.MSELoss()
-    optimizer = get_optimizer(param["optimizer"])
+    optimizer = get_optimizer(Adam)#param["optimizer"])
     optimizer = optimizer(
-        model.parameters(), lr=param["lr"]
+        model.parameters(), lr=cfg.scheduler.initial_lr#param["lr"]
     )
     scheduler = torch.optim.lr_scheduler.LambdaLR(
-        optimizer, lr_lambda=lambda step: param["decay_rate"]**step
+        optimizer, lr_lambda=lambda step: cfg.scheduler.decay_rate**step#param["decay_rate"]**step
     ) 
     #optimizer = torch.optim.Adam(model.parameters(), lr=cfg.scheduler.initial_lr)
     #scheduler = torch.optim.lr_scheduler.LambdaLR(
@@ -81,7 +82,7 @@ def run(job: RunningJob):
     objective = valLoss[-1]
     return {
         "objective": objective,
-        "metadata": {"TrainLoss": trainLoss, "valLoss": valLoss},
+        "metadata": {"TrainLoss": trainLoss, "ValLoss": valLoss},
     }
 
 if __name__ == '__main__':
@@ -113,44 +114,46 @@ if __name__ == '__main__':
     optimizers = ["Adadelta", "Adagrad", "Adam", "AdamW", "RMSprop", "SGD"]
     schedulers = ["cosine", "step"]
 
-    problem.add_hyperparameter((1, 16), "padding", default_value=9)
-    #problem.add_hyperparameter(activations, "lift_act", default_value="gelu")
+    #problem.add_hyperparameter((1, 16), "padding", default_value=9)
+    ##problem.add_hyperparameter(activations, "lift_act", default_value="gelu")
     problem.add_hyperparameter((2, 16), "fno_layers", default_value=4)
     problem.add_hyperparameter((2, 32), "fno_modes", default_value=12)
     problem.add_hyperparameter((2, 64), "latent_channels", default_value=32)
     problem.add_hyperparameter((1, 12), "decoder_layers", default_value=1)
     problem.add_hyperparameter((16, 48), "layer_size", default_value=32)
-    #problem.add_hyperparameter(activations, "proj_act", default_value="silu")
+    ##problem.add_hyperparameter(activations, "proj_act", default_value="silu")
 
-    problem.add_hyperparameter(optimizers, "optimizer", default_value="Adam")
-    problem.add_hyperparameter((1e-6, 1e-2), "lr", default_value=1e-3)
-    # problem.add_hyperparameter(schedulers, "scheduler", default_value='cosine')
-    problem.add_hyperparameter((16, 256), "batch_size", default_value=64)
-    #problem.add_hyperparameter((20, 100), 'epochs', default_value=20)
+    #problem.add_hyperparameter(optimizers, "optimizer", default_value="Adam")
+    #problem.add_hyperparameter((1e-6, 1e-2), "lr", default_value=1e-3)
+    ## problem.add_hyperparameter(schedulers, "scheduler", default_value='cosine')
+    ##problem.add_hyperparameter((16, 256), "batch_size", default_value=64)
+    ##problem.add_hyperparameter((20, 100), 'epochs', default_value=20)
     problem.add_hyperparameter((0.35, 0.95), "decay_rate", default_value=0.85)
     problem.add_hyperparameter((4, 16), "decay_epoch", default_value=8)
-    results = run(RunningJob(parameters=problem.default_configuration)) 
-    print("TrainLoss: ", results["metadata"]["TrainLoss"])
-    print("ValLoss: ", results["metadata"]["ValLoss"])
+    
+    # Baseline of training
+    #results = run(RunningJob(parameters=problem.default_configuration)) 
+    #print("TrainLoss: ", results["metadata"]["TrainLoss"])
+    #print("ValLoss: ", results["metadata"]["ValLoss"])
 
-    '''
+    print("problem: ", problem)    
     with Evaluator.create(
         run,
-        method="ray",
+        #method="mpicomm",
     ) as evaluator:
         if evaluator is not None:
             search = CBO(
                 problem,
                 evaluator,
-                moo_scalarization_strategy="Chebyshev",
-                moo_scalarization_weight="random",
-                objective_scaler="quantile-uniform",
-                acq_func="UCB",
-                multi_point_strategy="qUCB",
-                n_jobs=8,
-                verbose=1,
+                #moo_scalarization_strategy="Chebyshev",
+                #moo_scalarization_weight="random",
+                #objective_scaler="quantile-uniform",
+                #acq_func="UCB",
+                #multi_point_strategy="qUCB",
+                #n_jobs=1,
+                #verbose=1,
                 initial_points=[problem.default_configuration]
             )
-            results = search.search(max_evals=20)
+            results = search.search(max_evals=200)
             results.to_csv("results-1odes-stopper.csv")
-    '''
+   
